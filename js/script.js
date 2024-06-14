b10ff278db8a161ab14ecbb4b8416391e916af7b
elton asamoah <eltonasamoah007@gmail.com>
2024-06-14 12:18:57 +0000

    created facilities section and footer

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+
+// const btn1 = document.getElementById('hero-btn')
+// btn1.onclick =function() {
+//     alert('more soon');
+// }
+
+const btn1 = document.getElementById('hero-btn')
+btn1.onclick = function() {
+    alert('Online Booking Down. Please call')
+}
+
