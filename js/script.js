d88192dc86219be6b5fb97f841af4491aa07c8a2
elton asamoah <eltonasamoah007@gmail.com>
2024-06-14 13:04:04 +0000

    eddited footer

--- js/script.js
+++ js/script.js
@@ -1,12 +1,6 @@
 
-
-// const btn1 = document.getElementById('hero-btn')
-// btn1.onclick =function() {
-//     alert('more soon');
-// }
-
 const btn1 = document.getElementById('hero-btn')
 btn1.onclick = function() {
-    alert('Online Booking Down. Please call')
+    alert('Online Booking Down. Please call for assistance')
 }
 
